3f3cdd5546b02bd4a14bb83ac1430bec2dbde8f6
A. CO <64687788+Ayaan-Codes@users.noreply.github.com>
2020-11-20 14:16:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+console.log("Hey, Welcome to the ACO beta test")
+console.log("This data will be used to make the main SMARTi better, so the more you use it the more it helps out!")
+console.log("Note none of this data is saved and that this is only going to be used to make the AI speech and text better!")
+console.log("There is no need to fill real info, just anything to make the AI work.")
+console.log("----------------------")
+
+var dun=prompt("Welcome to ACO Official Website... to use the website you first need to verify you are not a robot... please type anything you want in the text area under to go on!")
+var myname=prompt("ACO Uses ACO Verification System™ to verify people... we have been able to verify you are human. Please type anything in the text area 1 last time please!")
+var myage=prompt("❗⛔🔴THE WEBSITE IS UNDER REPAIR🔴⛔❗... but please do not worry, you will still be able to use the website but just note the font may be small and the website may not like normal! (type anything to open up the website!)")
+
+
+..var like=prompt("How well do you like this bot?")
+..var howu=prompt("How are you doing?")
+..var favcol=prompt("What is your favorite color?")
+
+..console.log("Nice to meet you",myname)
+..console.log("Wow, you are",myage)
+..console.log("Thanks for the feedback! You said this AI was:",like)
+..console.log("Hope I can make it better! You said your day was:",howu)
+..console.log("I like it too, I will make your backround color as close as I can to it! You may also enter a custom color like #f20dfas. Im making it:",favcol)
+..console.log("Thank you for helping us!")
+..console.log("Each time you use this, we get to improve our app!")
 
